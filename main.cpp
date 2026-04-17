69b1a5eb2029446e7628cc55256b72a7f99b1209
Masymik-TP <kamilkopek90@gmail.com>
2026-04-17 13:14:36 +0200

    dodanie logiki liczenia fps i frame limiter

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <chrono>
+#include <thread>
+#include <windows.h>
 #include <iostream>
 
 // ---------========= Własne pliki nagłówkowe =========----------
@@ -7,9 +10,14 @@
 // ---------===========================================----------
 
 int main() {
+  // Ustawianie zmiennych środowiskowych
   Env env;
-  env.widowWidth = 800;
+  env.windowWidth = 800;
   env.windowHeight = 600;
+  env.desiredFPS = 144.0;
+  // ---=============================---
+
+  timeBeginPeriod(1);
 
   if (!glfwInit()) return -1;
 
@@ -17,7 +25,7 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(env.widowWidth, env.windowHeight, "Gra", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(env.windowWidth, env.windowHeight, "Gra", NULL, NULL);
 
   if (!window) {
     glfwTerminate();
@@ -25,18 +33,50 @@ int main() {
   }
   glfwMakeContextCurrent(window);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
-  glViewport(0, 0, env.widowWidth, env.windowHeight);
+  glViewport(0, 0, env.windowWidth, env.windowHeight);
+  glfwSwapInterval(0);
 
+  glClearColor((float)(135.0f / 255.0f), (float)(206.0f / 255.0f), (float)(235.0f / 255.0f), 1.0f);
+  std::chrono::steady_clock::time_point next_frame = std::chrono::steady_clock::now();
+  auto lastFrameTime = std::chrono::steady_clock::now();
+  auto frameTime = env.getFrameTime();
+  int frames = 0;
   // -----========= GŁÓWNA PĘTLA GRY =========-----
   while (!glfwWindowShouldClose(window)) {
-    glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+    // Logika FPS
+    auto frameStart = std::chrono::steady_clock::now();
+    double deltaTime = (frameStart - lastFrameTime).count();
+    lastFrameTime = frameStart;
+    // ---====---
+
     glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
+
+    // Frame limiter i liczenie FPS
+    auto now = std::chrono::steady_clock::now();
+    if (next_frame > now) {
+      std::this_thread::sleep_until(next_frame);
+    }
+    next_frame += std::chrono::duration_cast<std::chrono::steady_clock::duration>(frameTime);
+    frames++;
+    static auto lastFpsTime = std::chrono::steady_clock::now();
+
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - lastFpsTime)
+            .count();
+
+    if (elapsed >= 1.0) {
+      std::cout << frames << std::endl;
+      frames = 0;
+      lastFpsTime = std::chrono::steady_clock::now();
+    }
+    // ---====---
   }
   // -----====================================-----
 
+  timeEndPeriod(1);
   glfwTerminate();
   return 0;
 }
--- src/env.h
+++ src/env.h
@@ -1,6 +1,10 @@
 #pragma once
-#include <string.h>
+#include <chrono>
 class Env {
 public:
-  int windowHeight, widowWidth;
+  int windowHeight, windowWidth;
+  double desiredFPS;
+  std::chrono::duration<double, std::milli> getFrameTime() const {
+    return std::chrono::duration<double, std::milli>(1000.0 / desiredFPS);
+  }
 };
